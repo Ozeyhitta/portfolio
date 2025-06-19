73b76963febe464611c20bd71d70410928213d10
Ozeyhitta <150461966+Ozeyhitta@users.noreply.github.com>
2025-06-19 11:54:35 +0700

    header bar first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function myMenuFunction() {
+  //chuyen doi class cua phan tu
+  var menuBtn = document.getElementById("myNavMenu");
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
